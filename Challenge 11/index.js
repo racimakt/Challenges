ca5057e7c7504996ea5ad1bbc2c4120d8c7b438c
racimakt <racim.akt@gmail.com>
2025-09-15 07:59:11 +0100

    Add files via upload

---
+++ Challenge 11/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+// Middleware pour servir les fichiers statiques (CSS, JS, images)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "home.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "contact.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
